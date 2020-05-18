8c303d760dd2a5894309932c1e1d34b1f142ae92
LLEYTON MORRIS <lleyton.morris@non.agilent.com>
2020-05-18 16:57:56 +1000

    finished lexer potentially

--- GravyLang/Lexer.cs
+++ GravyLang/Lexer.cs
@@ -7,16 +7,34 @@ namespace GravyLang
 {
     class Lexer
     {
-
+        
 
         public string[] Lex(string languageInput)
         {
+            
+            char[] delimiters = { '{', '}', '(', ')', '=', '+', '/', '-', '.', '[', ']' };
+            char[] temp = {':', '}'};
 
-            char[] delimiters = {' ', '{', '}', '(', ')', '=', '+', '/', '-', '.', '[', ']' };
+            Console.WriteLine("Test case 1");
+            foreach (string str in inclusiveSplit("}}}", '}'))
+            {
+                Console.WriteLine(str);
+            }
 
-            string[] languageInputsArray = languageInput.Split(' ');
+            Console.WriteLine("Test case 2");
+            foreach (string str in inclusiveSplit("this is:} a : string that :} is : meaningless :::", ':'))
+            {
+                Console.WriteLine(str);
+            }
 
-            foreach (string str in inclusiveSplit("this is: a : string that : is : meaningless :::", ':')) 
+            Console.WriteLine("Test case 3");
+            foreach (string str in inclusiveSplit("} a", '}'))
+            {
+                Console.WriteLine(str);
+            }
+
+            Console.WriteLine("Test case recursive");
+            foreach (string str in inclusiveRecursiveSplit("this is:} a : string } that :} is : meaningless :::", temp.ToList())) 
             {
                 Console.WriteLine(str);
             }
@@ -26,55 +44,78 @@ namespace GravyLang
         private string[] inclusiveSplit(string input, char delimiter)
         {
             List<string> output = new List<string>();
-            List<int> occurences = new List<int>();
 
-            occurences.Add(-1);
+            int previousInstance = 0;
             for (int i = 0; i < input.Length; ++i)
-                if(input[i] == delimiter)
-                    occurences.Add(i);
-            occurences.Add(input.Length);
-
-            if(occurences.Any())
             {
-                for (int i = 1; i < occurences.Count; ++i)
+                if (input[i] == delimiter)
                 {
-                    output.Add(input.Substring(occurences[i - 1] + 1, occurences[i] - 1 - occurences[i - 1]));
-                    if(i != occurences.Count - 1)
+                    if (previousInstance == 0)
+                    {
+                        if (i == 0)
+                        {
+                            output.Add(delimiter.ToString());
+                            previousInstance = i + 1;
+                        }
+                        else
+                        {
+                            output.Add(input.Substring(0, i).Trim());
+                            output.Add(delimiter.ToString());
+                            previousInstance = i + 1;
+                        }
+                    }
+                    else
+                    {
+                        if (i != previousInstance)
+                            output.Add(input.Substring(previousInstance, i - previousInstance).Trim());
                         output.Add(delimiter.ToString());
+                        previousInstance = i + 1;
+                    }
                 }
-                return output.ToArray();
+                else
+                {
+                    //last index
+                    if (i == input.Length - 1 && i > previousInstance)
+                    {
+                        if (previousInstance != 0)
+                        {
+                            output.Add(input.Substring(previousInstance, i + 1 - previousInstance).Trim());
+                        }
+                        else
+                        {
+                            return new string[] { input };
+                        }
+                    }
+                }
             }
+
+            if(output.Any())
+                return output.ToArray();
 
             return new string[] { input };
         }
 
-        private string[] inclusiveRecursiveSplit(string input, char[] delimiters)
+        private string[] inclusiveRecursiveSplit(string input, List<char> delimiters)
         {
-            foreach(char chr in input)
+            List<string> result = inclusiveSplit(input, delimiters.First()).ToList();
+            delimiters.Remove(delimiters.First());
+
+            if(delimiters.Count == 0)
+                return result.ToArray();
+
+            List<string> finalResults = new List<string>();
+
+            if (result.Any())
             {
-                foreach(char del in delimiters)
+                for (int i = 0; i < result.Count; ++i)
                 {
-                    if(del == chr)
-                    {
-                        string[] split = input.Split(del);
-                        string[] output = Array.Empty<string>();
-
-                        //split
-                        //add del in correct positions
-
-                        foreach (string str in split) {
-                            if (str != del.ToString())
-                            {
-                                string[] temp = inclusiveRecursiveSplit(str, delimiters.Where(val => val != del).ToArray());
-                                output = output.Concat(temp).ToArray();
-                            }
-                        }
-                        //output = output.
-                        //return
-                    }
+                    string[] temp = inclusiveRecursiveSplit(result[i], new List<char>(delimiters));
+                    if (temp.Any())
+                        finalResults.AddRange(temp);
                 }
             }
-            return new string[] { input }; 
+
+            return finalResults.ToArray();
         }
     }
 }
